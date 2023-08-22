f8d84769524faba02a0daa1b1adc18235ade2551
Hamtana <hamtana.acc@gmail.com>
2023-08-22 13:31:31 -0700

    add header

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -3,8 +3,12 @@ import Image from "next/image"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Footer />
-    </main>
+    <>
+      <header></header>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+      <footer className="bg-slate-100 ">
+        <Footer />
+      </footer>
+    </>
   )
 }
