ee8cf220f9a4455ef926653f7525b0a259e2d0c2
Hamtana <hamtana.acc@gmail.com>
2023-08-23 16:21:25 +0330

    create header

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -7,7 +7,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <>
-      <header>
+      <header className=''>
         <Header />
       </header>
       <main className='flex min-h-screen flex-col items-center justify-between p-24'></main>
