cc3b0f1127faeb720fa2625fd7b7c1f2b1d3378e
Hamtana <hamtana.acc@gmail.com>
2023-08-23 14:25:59 -0700

    change header

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,12 +1,12 @@
 /** @format */
 import Footer from "@/components/footer"
-import Navbar from "@/components/header"
+import StickyHeader from "@/components/header"
 
 export default function Home() {
   return (
     <>
-      <header className="">
-        <Navbar />
+      <header>
+        <StickyHeader />
       </header>
       <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
       <footer className="bg-slate-100 ">
